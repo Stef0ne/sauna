1d62935a98c5b7e1ef42ac9b82950f34893b721c
Stef0ne <loko.roko1234567@gmail.com>
2021-10-25 20:23:13 +0300

    sauna
    sauna

--- js/main.js
+++ js/main.js
@@ -4,5 +4,8 @@
 // 	});
 // });
 $(function(){
-  $('.actual__slider').slick();
+  $('.actual__slider').slick({
+    prevArrow: '<img class="slider__arrow slider__arrow-left" src="images/arrow-left.svg" alt="arrow-left">',
+    nextArrow: '<img class="slider__arrow slider__arrow-right" src="images/arrow-right.svg" alt="arrow-right">',
+  });
 });
